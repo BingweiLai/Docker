1da38cce70314f10bb63cd61f90efa302013e58b
bing-wei <bing-wei@mail.chungyo.net>
2024-01-17 16:54:47 +0800

    docs: 整理文件

--- www/web2/rw.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$redis = new Redis();
-$redis->connect('192.16.0.20', 16379);
-$r = $redis->info();
-
-echo  $r['run_id'] . '<br>' . $r['role'] . '<br><br>';
-
-echo '<pre>', print_r($r), '</pre>';
